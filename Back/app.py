c0f2e85a367a1ed1fc369ff5b0484e1802d29ccb
Raincor5 <slava.krot5@gmail.com>
2023-12-03 19:16:29 +0000

    Before filtering implemented.

--- Back/app.py
+++ Back/app.py
@@ -1,11 +1,12 @@
 from requests import request
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/property_db"
 mongo = PyMongo(app)
-
+CORS(app)
 
 @app.route('/listings', methods=['GET'])
 def get_properties():
